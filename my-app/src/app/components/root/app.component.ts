775f2506652c3453fe4393c7e69b36852ea7badb
MSraoSuryawanshi <mnjptl20@gmail.com>
2022-07-31 11:34:47 +0530

    component with html and css

--- my-app/src/app/components/root/app.component.ts
+++ my-app/src/app/components/root/app.component.ts
@@ -2,20 +2,8 @@ import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: '.app-root',
-  template: `
-    <h1>Welcome to Angular</h1>
-    <p>hello from para</p>
-  `,
-  styles: [
-    `
-      h1 {
-        color: hotpink;
-      }
-      p {
-        color: green;
-      }
-    `,
-  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class FirstComponent {
   // data
