f0d139453c52d171c34e03e85e5324287e289ae3
MSraoSuryawanshi <mnjptl20@gmail.com>
2022-08-06 12:00:09 +0530

    components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FirstComponent } from './components/root/app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BannerOneComponent } from './components/banner-one/banner-one.component';
+import { BannerTwoComponent } from './components/banner-two/banner-two.component';
+import { BannerThreeComponent } from './components/banner-three/banner-three.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
-  declarations: [FirstComponent, HeaderComponent, FooterComponent], // list of Compo, . . .
+  declarations: [
+    FirstComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerOneComponent,
+    BannerTwoComponent,
+    BannerThreeComponent,
+    ContactUsComponent,
+    ProductsComponent,
+  ], // list of Compo, . . .
   imports: [BrowserModule],
   bootstrap: [FirstComponent],
 })
