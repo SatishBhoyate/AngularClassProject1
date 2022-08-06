22c54c57da6ca17d41d6c3b229838215a97ce26d
MSraoSuryawanshi <mnjptl20@gmail.com>
2022-08-06 11:30:36 +0530

    components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Component, NgModule } from '@angular/core';
-import { FirstComponent } from './components/root/app.component';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FirstComponent } from './components/root/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
-  declarations: [FirstComponent], // list of Compo, . . .
+  declarations: [FirstComponent, HeaderComponent, FooterComponent], // list of Compo, . . .
   imports: [BrowserModule],
   bootstrap: [FirstComponent],
 })
