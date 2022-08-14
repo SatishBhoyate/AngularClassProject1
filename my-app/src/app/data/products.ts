5ef27f30eb68c8eef04456cbcfccb93c5ad3b617
MSraoSuryawanshi <mnjptl20@gmail.com>
2022-08-14 11:42:57 +0530

    mock, model

---
+++ my-app/src/app/data/products.ts
@@ -0,0 +1,25 @@
+import { Products } from './../model/products.model';
+// const PI
+export const PRODUCTS: Products[] = [
+  {
+    prdName: 'Mobile',
+    prdId: 1001,
+    inStock: 8,
+    country: 'India',
+    prdImg: './../../assets/images/sample-1.jpg',
+  },
+  {
+    prdName: 'Laptop',
+    prdId: 1002,
+    inStock: 10,
+    country: 'Australia',
+    prdImg: './../../assets/images/sample-1.jpg',
+  },
+  {
+    prdName: 'Book',
+    prdId: 1003,
+    inStock: 0,
+    country: '',
+    prdImg: './../../assets/images/sample-1.jpg',
+  },
+];
